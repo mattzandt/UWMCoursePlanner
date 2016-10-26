5c95ab841919bf2d1dd851b7d528f9219a33cecc
mattzandt <mattzandt@gmail.com>
2016-10-26 16:20:11 -0500

    Added major & minor requests

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,4 +5,10 @@ var ctrlMain = require('../controllers/main'); // require main.js file in contro
 /* GET home page. */
 router.get('/', ctrlMain.index); // in route definition, references index method of the external controller file
 
+/* GET list of majors */
+router.get('/majors', ctrlMain.majors);
+
+/* GET list of minors */
+router.get('/minors', ctrlMain.minors);
+
 module.exports = router; // expose router constructor?
