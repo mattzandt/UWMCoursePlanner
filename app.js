f9efa8fe46b617448f6680806905fe41a88b9877
yangxie@uwm.edu <yangxie@uwm.edu>
2016-10-26 17:08:11 -0500

    Make graph somewhat vertically responsive

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
